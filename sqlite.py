41df4845c3fca8461b44076d6ebd7c785114f9c9
renevegacom <renevega.com@hotmail.com>
2016-09-23 07:02:40 -0600

    archivo python para crear base de datos en memoria de la pc

---
+++ sqlite.py
@@ -0,0 +1,33 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#http://www.pythondiario.com/2013/12/python-y-sqlite3-como-base-de-datos.html
+
+import sqlite3
+
+con = sqlite3.connect('C:\Python27\pruebas\prueba')
+#coneccion = sqlite3.connect(':memory:')
+
+cursor = con.cursor()
+
+print u"La base de datos se abrio correctamente"
+
+cursor.execute( ''' CREATE TABLE EMPRESA
+        (ID INT PRIMARY KEY       NOT NULL,
+         NOMBRE         TEXT	  NOT NULL,
+         EDAD           INT       NOT NULL,
+         DIRECCION      CHART(50),
+		 SALARIO        REAL)''')
+
+print u"Tabla creada con exito"
+
+
+
+
+
+
+
+
+
+
+
+
